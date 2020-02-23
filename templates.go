2fccb6d112e6a810195bce18706a2cb6aee75104
Parker Moore <parkrmoore@gmail.com>
2020-02-23 15:21:09 -0500

    Forward Host Header

--- templates.go
+++ templates.go
@@ -66,6 +66,7 @@ var rootLocationTmpl = template.Must(rootTmpl.New("rootLocation").Parse(strings.
         {{else if .IsProxy}}proxy_pass {{.ProxyURL}};
         proxy_set_header  X-Real-IP  $remote_addr;
         proxy_set_header  X-Forwarded-For  $remote_addr;
+        proxy_set_header  Host $host;
         {{end}}
     }
 `)))
@@ -93,6 +94,7 @@ var mediaLocationTmpl = template.Must(rootTmpl.New("mediaLocation").Parse(string
         proxy_pass {{.ProxyURL}};
         proxy_set_header  X-Real-IP  $remote_addr;
         proxy_set_header  X-Forwarded-For  $remote_addr;
+        proxy_set_header  Host $host;
         {{end}}
     }
 `)))
